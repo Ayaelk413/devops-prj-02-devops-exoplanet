5f0f27d1837bb3a14195851fbce122ca4e0077b6
Sikawie <113896877+Sikawie@users.noreply.github.com>
2022-12-21 23:02:40 +0100

    fix(uppercase.js): fixed regex

--- utils/uppercase.js
+++ utils/uppercase.js
@@ -1,5 +1,5 @@
 module.exports.uppercase = (uniqueName) => {
-    const regex = /[A-Z]/
+    const regex = /[a-z]{1,}/
     const found = uniqueName.match(regex)
     console.log(found)
     if (found === null || found.length === 0) return false
