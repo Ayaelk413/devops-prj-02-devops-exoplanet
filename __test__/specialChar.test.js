f4e6862c88f9796e3f26f4ca75723f1d66945efb
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-21 10:36:58 +0100

    test(specialchar): strengthen the example test

--- __test__/specialChar.test.js
+++ __test__/specialChar.test.js
@@ -2,17 +2,17 @@ const { character } = require('../utils/character');
 
 describe("app tests chars - character", () => {
     test('should return true as the character is correct', () => {
-        expect(character('test')).toBe(true);
+        expect(character('Trappiste01-00')).toBe(true);
     });
     test('should return false as there is special character', () => {
-        expect(character('te/st@')).toBe(false);
+        expect(character('Trappiste@****01/.[]{}/\()?!-`\'"')).toBe(false);
     });
     
     test('should return true as the special character _ is accepted', () => {
-        expect(character('Te-st')).toBe(true);
+        expect(character('Trappiste01-00')).toBe(true);
     });
     test('should return true as the special character . is accepted', () => {
-        expect(character('te.St')).toBe(true);
+        expect(character('Trappiste.01.00')).toBe(true);
     });
     
     test('should return false as the parameter is empty', () => {
--- utils/character.js
+++ utils/character.js
@@ -1,6 +1,6 @@
 module.exports.character = (uniqueName) => {
     if(!uniqueName) return false;
-    const regex = /[[_@*+?^${}()|[\]\\]/g;
+    const regex = /[[_@*+!`'"?^${}()|[\]\\]/g;
     const found = uniqueName.match(regex);
     console.log(found);
     if (found === null) return true;
