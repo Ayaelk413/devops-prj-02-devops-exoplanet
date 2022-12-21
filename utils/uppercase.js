a7d1139315231ba1ef14fab3aee64c6a3ff9070f
Sikawie <113896877+Sikawie@users.noreply.github.com>
2022-12-21 23:34:34 +0100

    refactor(uppercase.js): refactored return

--- utils/uppercase.js
+++ utils/uppercase.js
@@ -2,5 +2,5 @@ module.exports.uppercase = (uniqueName) => {
     if (!uniqueName) return false
     const regex = /[a-z]{1,}/
     const found = uniqueName.match(regex)
-    return found === null
+    return !found
 }
