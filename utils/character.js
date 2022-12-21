5719b325e3d2e604de6b926ffc06b9877639a444
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-21 11:35:58 +0100

    refactor(specialchar): refactor character

--- utils/character.js
+++ utils/character.js
@@ -2,7 +2,5 @@ module.exports.character = (uniqueName) => {
     if(!uniqueName) return false;
     const regex = /[a-z]{1,}|[[_@*+!`'" ?^${}()|[\]\\]/g;
     const found = uniqueName.match(regex);
-    console.log(found);
-    if (found === null) return true;
-    else return false;
+    return !found;
 }
