8d87a1a026577b7bcc3594acaf77e6d669cc6476
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-21 11:31:01 +0100

    test(specialchar): fixed regex for small caps

--- __test__/specialChar.test.js
+++ __test__/specialChar.test.js
@@ -2,17 +2,17 @@ const { character } = require('../utils/character');
 
 describe("app tests chars - character", () => {
     test('should return true as the character is correct', () => {
-        expect(character('Trappiste01-00')).toBe(true);
+        expect(character('TRAPPISTE01-00')).toBe(true);
     });
     test('should return false as there is special character', () => {
-        expect(character('Trappiste@****01/.[]{}/\()?!-`\' "')).toBe(false);
+        expect(character('TRAPPISTE@****01/.[]{}/\()?!-`\' "')).toBe(false);
     });
     
     test('should return true as the special character _ is accepted', () => {
-        expect(character('Trappiste01-00')).toBe(true);
+        expect(character('TRAPPISTE01-00')).toBe(true);
     });
     test('should return true as the special character . is accepted', () => {
-        expect(character('Trappiste.01.00')).toBe(true);
+        expect(character('TRAPPISTE.01.00')).toBe(true);
     });
 
     test('should return false as there is a white space', () => {
--- utils/character.js
+++ utils/character.js
@@ -1,6 +1,6 @@
 module.exports.character = (uniqueName) => {
     if(!uniqueName) return false;
-    const regex = /[[_@*+!`'" ?^${}()|[\]\\]/g;
+    const regex = /[a-z]{1,}|[[_@*+!`'" ?^${}()|[\]\\]/g;
     const found = uniqueName.match(regex);
     console.log(found);
     if (found === null) return true;
