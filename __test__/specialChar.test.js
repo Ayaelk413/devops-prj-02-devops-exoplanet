7cb4994755e9a9aa7d45674a8030120b5ce1db4b
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-21 10:41:30 +0100

    test(specialchar): adding test return false if there is a white space

--- __test__/specialChar.test.js
+++ __test__/specialChar.test.js
@@ -5,7 +5,7 @@ describe("app tests chars - character", () => {
         expect(character('Trappiste01-00')).toBe(true);
     });
     test('should return false as there is special character', () => {
-        expect(character('Trappiste@****01/.[]{}/\()?!-`\'"')).toBe(false);
+        expect(character('Trappiste@****01/.[]{}/\()?!-`\' "')).toBe(false);
     });
     
     test('should return true as the special character _ is accepted', () => {
@@ -13,6 +13,10 @@ describe("app tests chars - character", () => {
     });
     test('should return true as the special character . is accepted', () => {
         expect(character('Trappiste.01.00')).toBe(true);
+    });
+
+    test('should return false as there is a white space', () => {
+        expect(character(' ')).toBe(false);
     });
     
     test('should return false as the parameter is empty', () => {
