98d35903cebeacbf7d2ea5daa58053db0dd1a894
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-21 01:37:48 +0100

    test(empty parameter): empty parameter

--- utils/character.js
+++ utils/character.js
@@ -1,7 +1,8 @@
 module.exports.character = (uniqueName) => {
-    const regex = /\W+\s/g
-    const found = uniqueName.match(regex)
-    console.log(found)
-    if (found === null || found.length === 0) return true
-    else return false
+    if(!uniqueName) return false;
+    const regex = /\W+\s/g;
+    const found = uniqueName.match(regex);
+    console.log(found);
+    if (found === null || found.length === 0) return true;
+    else return false;
 }
