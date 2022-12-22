92878c85b11b9c90127502142bd9d3b674b53b6a
chehr <chehrazad.ouazzani@student.vinci.be>
2022-12-22 21:58:17 +0100

    refactor(uppercase test): edited the code to make it more easier to read

--- __test__/uppercase.test.js
+++ __test__/uppercase.test.js
@@ -3,23 +3,28 @@ const { uppercase } = require('../utils/uppercase')
 describe('app tests - uppercase', () => {
     
     test('should return true as there are only uppercase characters', () => {
-        expect(uppercase('TRAPPISTE')).toBe(true)
+        const result = uppercase('TRAPPISTE');
+        expect(result).toBe(true)
     })
 
     test('should return false as there is a lowercase character', () => {
-        expect(uppercase('TRAPPiSTE')).toBe(false)
+        const result = uppercase('TRAPPiSTE');
+        expect(result).toBe(false)
     })
 
     test('should return false as there are multiple lowercase characters', () => {
-        expect(uppercase('Trappist')).toBe(false)
+        const result = uppercase('Trappist');
+        expect(result).toBe(false)
     })
 
     test('should return false as there are no characters', () => {
-        expect(uppercase('')).toBe(false)
+        const result = uppercase('');
+        expect(result).toBe(false)
     })
 
     test('should return false as there are only lowercase characters', () => {
-        expect(uppercase('trappist')).toBe(false)
+        const result = uppercase('trappist');
+        expect(result).toBe(false)
     })
 
 })
